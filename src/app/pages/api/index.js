af58717d9c7ce214fef4d8a6e900b043536c0138
kasvrol <dovalekarolina@gmail.com>
2023-09-12 00:43:51 -0300

    feat(spi): configuração de api em produção

--- src/app/pages/api/index.js
+++ src/app/pages/api/index.js
@@ -10,13 +10,29 @@ export async function API_PROGRAMACAO(data) {
     };
     try {
         const response = await axios.get(
-            `https://epg-api.video.globo.com/programmes/1337?date=${data}`,
+            `https://epg-api.video.globo.com/programmes/d1337?date=${data}`,
             {
                 headers,
             }
         );
         return response.data;
     } catch (error) {
-        throw new Error(error);
+        console.error("Erro ao fazer a requisição:", error.message);
+
+        try {
+            const alternativeResponse = await axios.get(
+                `https://grade-de-programacao-back-end.vercel.app/programas/${data}`,
+                {
+                    headers,
+                }
+            );
+            return alternativeResponse.data;
+        } catch (errorAlternativeResponse) {
+            console.error(
+                "Erro na requisição alternativa:",
+                errorAlternativeResponse.message
+            );
+            throw new Error("Erro na requisição alternativa");
+        }
     }
 }
